5dede5625e9fc9bb205cc9ce8f93b1d802cd283f
mehok <mehok@Osamah-Abdullah>
2023-08-06 23:31:05 +0300

    123

--- src/main/java/sakancomMain/Skankom.java
+++ src/main/java/sakancomMain/Skankom.java
@@ -50,10 +50,7 @@ public class Skankom implements Serializable {
 		ObjectOutputStream out = null;
 		try {
 			fileOut= new FileOutputStream("instance.ser");
-			try {
             out = new ObjectOutputStream(fileOut);
-            }
-			catch(IOException e) {}
             out.writeObject(getInstance());
             out.close();
             fileOut.close();
@@ -76,11 +73,7 @@ public class Skankom implements Serializable {
 
 		try {
 			 fileIn = new FileInputStream("instance.ser");
-			 try {
 			 in = new ObjectInputStream(fileIn);
-			 }
-			 catch(IOException e) {}
-			 
 			deserializedInstance = (Skankom) in.readObject();
 			in.close();
 			fileIn.close();
