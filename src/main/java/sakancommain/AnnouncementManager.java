38ac134a882f808e84b58104e7a75268b672bd68
mehok <mehok@Osamah-Abdullah>
2023-08-08 10:26:33 +0300

    T1

--- src/main/java/sakancommain/AnnouncementManager.java
+++ src/main/java/sakancommain/AnnouncementManager.java
@@ -15,7 +15,7 @@ public class AnnouncementManager {
 		Collection<Announcement> collection = skankom.getAnnouncements().values();
 		Announcement[] announcements = collection.toArray(new Announcement[0]);
 		if (announcements.length == 0) {
-			LOGGER.log(Level.INFO,noan);
+			LOGGER.log(Level.INFO,NOAN);
 			AdminManager.enterAsAdmin(admin);
 			return;
 		}
@@ -49,7 +49,7 @@ public class AnnouncementManager {
 		ArrayList<Announcement> announcements = skankom.getAnnouncements(owner);
 
 		if (announcements.isEmpty()) {
-			LOGGER.log(Level.INFO,noan);
+			LOGGER.log(Level.INFO,NOAN);
 			OwnerManager.enterAsOwner(owner);
 			return;
 		}
@@ -77,12 +77,12 @@ public class AnnouncementManager {
 			SakancomApp.entrance();
 		}
 	}
-	private static final String noan="No announcements to show";
+	private static final String NOAN="No announcements to show";
 	public static void showAnnouncements(Tenant tenant) throws IOException {
 		ArrayList<Announcement> arrayList = skankom.getAcceptedAnnouncements();
 		Announcement[] announcements = arrayList.toArray(new Announcement[0]);
 		if (announcements.length == 0) {
-			LOGGER.log(Level.INFO,noan);
+			LOGGER.log(Level.INFO,NOAN);
 			TenantManager.enterAsTenant(tenant);
 			return;
 		}
