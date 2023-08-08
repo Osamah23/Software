eab060118a0b07e5e6e1652860fedfc5b71f0cef
mehok <mehok@Osamah-Abdullah>
2023-08-08 18:32:30 +0300

    wq

--- src/main/java/sakancommain/TenantManager.java
+++ src/main/java/sakancommain/TenantManager.java
@@ -1,33 +1,37 @@
 package sakancommain;
 
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import owner.*;
 
 public class TenantManager {
 	private static Skankom skankom = Skankom.getInstance();
 	private TenantManager() {}
+	private static final Logger LOGGER = Logger.getLogger(TenantManager.class.getName());
 
 	public static void createTenant(User user) throws IOException {
-		System.out.println("Enter Tenant Name:");
+		LOGGER.log(Level.INFO,"Enter Tenant Name:");
 		String tenantName = SakancomApp.getScanner().nextLine();
-		System.out.println("Enter University Major:");
+		LOGGER.log(Level.INFO,"Enter University Major:");
 		String universityMajor = SakancomApp.getScanner().nextLine();
-		System.out.println("Enter Age:");
+		LOGGER.log(Level.INFO,"Enter Age:");
 		int age = SakancomApp.scanInt();
-		System.out.println("Enter Email:");
+		LOGGER.log(Level.INFO,"Enter Email:");
 		String email = SakancomApp.getScanner().nextLine();
-		System.out.println("Enter Phone Number:");
+		LOGGER.log(Level.INFO,"Enter Phone Number:");
 		String phoneNumber = SakancomApp.getScanner().nextLine();
 		Tenant tenant = new Tenant(tenantName, email, phoneNumber, universityMajor, age);
 		user.setUserId(tenant.getId());
 		skankom.addTenant(tenant);
 		skankom.addUser(user);
-		System.out.println(tenant.toString() + " is created.");
+		LOGGER.log(Level.INFO,tenant.toString() + " is created.");
 		enterAsTenant(tenant);
 	}
 
 	public static void enterAsTenant(Tenant tenant) throws IOException {
-		System.out.println("Choose one of the following options:\n1) Show Announecemnts \n2) Manage Reservations \n3) Manage Booking \n4) Manage Furniture \n5) Exit");
+		LOGGER.log(Level.INFO,"Choose one of the following options:\n1) Show Announecemnts \n2) Manage Reservations \n3) Manage Booking \n4) Manage Furniture \n5) Exit");
 		int choice = SakancomApp.scanInt();
 		switch (choice) {
 		case 1:
@@ -53,25 +57,25 @@ public class TenantManager {
 
 	public static void manageBooking(Tenant tenant) throws IOException {
 		if (tenant.getBookedHousing() == null) {
-			System.out.println("You haven't booked any apartment yet.");
+			LOGGER.log(Level.INFO,"You haven't booked any apartment yet.");
 			return;
 		}
-		System.out.println("Choose one of the following options:\n1) Show Booking Details \n2) Remove Booking \n3) Return Back \n4) Exit");
+		LOGGER.log(Level.INFO,"Choose one of the following options:\n1) Show Booking Details \n2) Remove Booking \n3) Return Back \n4) Exit");
 		int choice = SakancomApp.scanInt();
 		switch (choice) {
 		case 1:
-			System.out.println(tenant.toString());
+			LOGGER.log(Level.INFO,tenant.toString());
 			Housing bookedHousing = skankom.getHousing(tenant.getBookedHousing());
 			if (bookedHousing != null) {
 				Owner owner = skankom.getOwner(bookedHousing.getOwnerId());
 				if (owner != null) {
-					System.out.println(owner.toString());
+					LOGGER.log(Level.INFO,owner.toString());
 				}
 			}
-			System.out.println("Time to pay rent: " + tenant.getTimeToPay());
+			LOGGER.log(Level.INFO,"Time to pay rent: " + tenant.getTimeToPay());
 			break;
 		case 2:
-			System.out.println("Delete Booking Confirmation:\n1) Confirm \n2) Cancel");
+			LOGGER.log(Level.INFO,"Delete Booking Confirmation:\n1) Confirm \n2) Cancel");
 			choice = SakancomApp.scanInt();
 			if (choice == 1) {
 				Housing housing = skankom.getHousing(tenant.getBookedHousing());
@@ -79,7 +83,7 @@ public class TenantManager {
 					housing.removeTenant(tenant);
 				}
 				tenant.removebBookedHousing();
-				System.out.println("Booked Apartment is deleted successflly");
+				LOGGER.log(Level.INFO,"Booked Apartment is deleted successflly");
 			}
 			enterAsTenant(tenant);
 			break;
