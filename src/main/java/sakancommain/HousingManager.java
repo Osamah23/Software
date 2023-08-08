2fd9bac67b848dd2d436947b8e704f93a6ee08a3
mehok <mehok@Osamah-Abdullah>
2023-08-08 11:47:00 +0300

    a

--- src/main/java/sakancommain/HousingManager.java
+++ src/main/java/sakancommain/HousingManager.java
@@ -2,32 +2,36 @@ package sakancommain;
 
 import java.io.IOException;
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import owner.*;
 
 public class HousingManager {
 	private static Skankom skankom = Skankom.getInstance();
 	private HousingManager() {}
+	private static final Logger LOGGER = Logger.getLogger(HousingManager.class.getName());
 
 	public static Housing createHousing(Owner owner) throws IOException {
-		System.out.println("Enter Location:");
+		LOGGER.log(Level.INFO,"Enter Location:");
 		String location = SakancomApp.getScanner().nextLine();
-		System.out.println("Does it student apartment? \n1) Yes\n2) No");
+		LOGGER.log(Level.INFO,"Does it student apartment? \n1) Yes\n2) No");
 		boolean isStudentApartment = SakancomApp.scanInt() == 1;
 		Housing housing = new Housing(owner.getId(), location, isStudentApartment);
 		housing = createHousingDetails(housing);
-		System.out.println("Adding Housing Confirmation:\n1) Confirm \n2) Cancel");
+		LOGGER.log(Level.INFO,"Adding Housing Confirmation:\n1) Confirm \n2) Cancel");
 		int choice = SakancomApp.scanInt();
 		if (choice == 1) {
 			owner.addHousing(housing);
 			skankom.addHousing(housing);
-			System.out.println("Housing with: " + housing.toString() + " is added.");
+			LOGGER.log(Level.INFO,"Housing with: " + housing.toString() + " is added.");
 			return housing;
 		}
 		return null;
 	}
 
 	public static Housing createHousingDetails(Housing housing) throws IOException {
-		System.out.println("Choose one of the following options:\n1) Manage Floors \n2) Show Housing Details \n3) Edit Location \n4) Finish");
+		LOGGER.log(Level.INFO,"Choose one of the following options:\n1) Manage Floors \n2) Show Housing Details \n3) Edit Location \n4) Finish");
 		int choice = SakancomApp.scanInt();
 		switch (choice) {
 		case 1:
@@ -37,14 +41,14 @@ public class HousingManager {
 			housing.viewHousingStatistics();
 			return createHousingDetails(housing); 
 		case 3:
-			System.out.println("Enter New Location:");
+			LOGGER.log(Level.INFO,"Enter New Location:");
 			String location = SakancomApp.getScanner().nextLine();
 			housing.setLocation(location);
 			return createHousingDetails(housing);
 		case 4:
 			return housing;
 		default:
-			System.out.println("Wrong choice.\n1) Try Again 2) Finish");
+			LOGGER.log(Level.INFO,"Wrong choice.\n1) Try Again 2) Finish");
 			if (SakancomApp.scanInt() == 1) {
 				return createHousingDetails(housing);
 			}
@@ -60,21 +64,21 @@ public class HousingManager {
 		Collection<Housing> collection = skankom.getHousings().values();
 		Housing[] housings = collection.toArray(new Housing[0]);
 		if (housings.length == 0) {
-			System.out.println("No hosuings to show");
+			LOGGER.log(Level.INFO,"No hosuings to show");
 			AdminManager.enterAsAdmin(admin);
 			return;
 		}
 		int counter = 1;
 		for (Housing housing: housings) {
-			System.out.println(counter + ") " + housing.toString());
+			LOGGER.log(Level.INFO,counter + ") " + housing.toString());
 			String ownerId = housing.getOwnerId();
 			Owner owner = skankom.getOwner(ownerId);
 			if (owner != null) {
-				System.out.println(owner.toString());
+				LOGGER.log(Level.INFO,owner.toString());
 			}
 			counter++;
 		}
-		System.out.println("Choose one of the following options:\n1) Edit Housing \n2) Remove Housing \n3) Return Back \n4) Exit");
+		LOGGER.log(Level.INFO,"Choose one of the following options:\n1) Edit Housing \n2) Remove Housing \n3) Return Back \n4) Exit");
 		int choice = SakancomApp.scanInt();
 		switch (choice) {
 		case 1:
@@ -96,16 +100,16 @@ public class HousingManager {
 		ArrayList<Housing> arrayList = skankom.getHousings(owner);
 		Housing[] housings = arrayList.toArray(new Housing[0]);
 		if (housings.length == 0) {
-			System.out.println("No hosuings to show");
+			LOGGER.log(Level.INFO,"No hosuings to show");
 			OwnerManager.enterAsOwner(owner);
 			return;
 		}
 		int counter = 1;
 		for (Housing housing: housings) {
-			System.out.println(counter + ") " + housing.toString());
+			LOGGER.log(Level.INFO,counter + ") " + housing.toString());
 			counter++;
 		}
-		System.out.println("Choose one of the following options:\n1) Edit Housing \n2) Remove Housing \n3) Return Back \n4) Exit");
+		LOGGER.log(Level.INFO,"Choose one of the following options:\n1) Edit Housing \n2) Remove Housing \n3) Return Back \n4) Exit");
 		int choice = SakancomApp.scanInt();
 		switch (choice) {
 		case 1:
@@ -124,16 +128,16 @@ public class HousingManager {
 	}
 
 	public static void editHousing(Housing[] housings) throws IOException {
-		System.out.println("Enter Housing Number:");
+		LOGGER.log(Level.INFO,"Enter Housing Number:");
 		int housingNumber = SakancomApp.scanInt();
 		if (housingNumber <= housings.length) {
 			Housing housing = housings[housingNumber - 1];
 			housing = editHousing(housing);
 			if (housing != null) {
-				System.out.println("Housing with: " + housing.toString() + " is edited." );
+				LOGGER.log(Level.INFO,"Housing with: " + housing.toString() + " is edited." );
 			}
 		} else {
-			System.out.println("The entered number is not exist.\n1) Try Again 2) Cancel");
+			LOGGER.log(Level.INFO,"The entered number is not exist.\n1) Try Again 2) Cancel");
 			if (SakancomApp.scanInt() == 1) {
 				editHousing(housings);
 			}
@@ -141,16 +145,16 @@ public class HousingManager {
 	}
 
 	public static void removeHousing(Housing[] housings) throws IOException {
-		System.out.println("Enter Housing Number:");
+		LOGGER.log(Level.INFO,"Enter Housing Number:");
 		int housingNumber = SakancomApp.scanInt();
 		if (housingNumber > housings.length) {
-			System.out.println("The entered number is not exist.\n1) Try Again 2) Cancel");	
+			LOGGER.log(Level.INFO,"The entered number is not exist.\n1) Try Again 2) Cancel");	
 			if (SakancomApp.scanInt() == 1) {
 				removeHousing(housings);
 			}
 			return;
 		}
-		System.out.println("1) Remove \n2) Cancel");
+		LOGGER.log(Level.INFO,"1) Remove \n2) Cancel");
 		Boolean remove = SakancomApp.scanInt() == 1;
 		if (!remove) { return; }
 		Housing housing = housings[housingNumber - 1];
@@ -159,6 +163,6 @@ public class HousingManager {
 			owner.removeHousing(housing);
 		}
 		skankom.removeHousing(housing);
-		System.out.println(housing.toString() + " is deleted");
+		LOGGER.log(Level.INFO,housing.toString() + " is deleted");
 	}
 }
