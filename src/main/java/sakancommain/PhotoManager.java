e37b71b7fe0ef9b64bba59ec5694ff1462b844dc
mehok <mehok@Osamah-Abdullah>
2023-08-08 12:36:30 +0300

    saas

--- src/main/java/sakancommain/PhotoManager.java
+++ src/main/java/sakancommain/PhotoManager.java
@@ -2,6 +2,8 @@ package sakancommain;
 
 import java.io.*;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.awt.image.BufferedImage;
 
 import javax.imageio.*;
@@ -10,9 +12,9 @@ import owner.*;
 public class PhotoManager {
 	private static Skankom skankom = Skankom.getInstance();
 	private PhotoManager() {}
-
+	 private static final Logger LOGGER = Logger.getLogger(PhotoManager.class.getName());
 	public static void managePhotos(Announcement announcement) throws IOException {
-		System.out.println("Choose one of the following options:\n1) Add Photo \n2) Remove Photo \n3) Show Photos \n4) Finish");
+		LOGGER.log(Level.INFO,"Choose one of the following options:\n1) Add Photo \n2) Remove Photo \n3) Show Photos \n4) Finish");
 		int choice = SakancomApp.scanInt();
 		switch (choice) {
 		case 1:
@@ -30,7 +32,7 @@ public class PhotoManager {
 		case 4:
 			break;
 		default:
-			System.out.println("Wrong choice.\n1) Try Again 2) Finish");
+			LOGGER.log(Level.INFO,"Wrong choice.\n1) Try Again 2) Finish");
 			if (SakancomApp.scanInt() == 1) {
 				managePhotos(announcement);
 			}
@@ -38,15 +40,15 @@ public class PhotoManager {
 	}
 	
 	public static void addPhoto(Announcement announcement) {
-		System.out.println("Enter Photo URL:");
+		LOGGER.log(Level.INFO,"Enter Photo URL:");
 		String url = SakancomApp.getScanner().nextLine();
 		try {
 			BufferedImage source = ImageIO.read(new FileInputStream((url)));
 			announcement.addPhoto(url);
-			System.out.println("Your photo added successfully");
+			LOGGER.log(Level.INFO,"Your photo added successfully");
 		} 
 		 catch (IOException e) {
-			System.out.println("error in adding the photo \n1) Try Again \n2) Cancel");
+			LOGGER.log(Level.INFO,"error in adding the photo \n1) Try Again \n2) Cancel");
 			if (SakancomApp.scanInt() == 1) {
 				addPhoto(announcement);
 			}
@@ -56,7 +58,7 @@ public class PhotoManager {
 	public static void showPhotos(Announcement announcement) {
 		List<BufferedImage> photos = announcement.getPhotos();
 		if (photos.isEmpty()) {
-			System.out.println("No photos added yet to " + announcement.toString());
+			LOGGER.log(Level.INFO,"No photos added yet to " + announcement.toString());
 			return;
 		}
 		PhotoShower photoShower = new PhotoShower();
@@ -64,29 +66,29 @@ public class PhotoManager {
 	}
 	
 	public static void removePhoto(int photosCount, Announcement announcement) throws IOException {
-		System.out.println("Enter Photo Number:");
+		LOGGER.log(Level.INFO,"Enter Photo Number:");
 		int photoNumber = SakancomApp.scanInt();
 		if (photoNumber > photosCount) {
-			System.out.println("The entered number is not exist.\n1) Try Again \n2) Cancel");
+			LOGGER.log(Level.INFO,"The entered number is not exist.\n1) Try Again \n2) Cancel");
 			if (SakancomApp.scanInt() == 1) {
 				removePhoto(photosCount, announcement);
 			}
 			return;
 		}
-		System.out.println("1) Remove \n2) Cancel");
+		LOGGER.log(Level.INFO,"1) Remove \n2) Cancel");
 		Boolean remove = SakancomApp.scanInt() == 1;
 		if (!remove) { return; }
 		announcement.removePhoto(photoNumber - 1);
-		System.out.println("Your Photo deleted successfully");
+		LOGGER.log(Level.INFO,"Your Photo deleted successfully");
 	}
 	
 	public static void deletePhoto(Announcement announcement) throws IOException {
 		int photosCount = announcement.getPhotosCount();
 		if (photosCount == 0) {
-			System.out.println("No photos added yet to " + announcement.toString());
+			LOGGER.log(Level.INFO,"No photos added yet to " + announcement.toString());
 			return;
 		}
-		System.out.println("There is " + photosCount + " photos in the announcement.");
+		LOGGER.log(Level.INFO,"There is " + photosCount + " photos in the announcement.");
 		removePhoto(photosCount, announcement);
 	}
 }
