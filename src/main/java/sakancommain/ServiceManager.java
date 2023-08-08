43007e60254a0bd8076cc3609fa79f5ff774a1e9
mehok <mehok@Osamah-Abdullah>
2023-08-08 18:08:03 +0300

    wr

--- src/main/java/sakancommain/ServiceManager.java
+++ src/main/java/sakancommain/ServiceManager.java
@@ -2,27 +2,31 @@ package sakancommain;
 
 import java.io.IOException;
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import owner.*;
 
 public class ServiceManager {
 	private static Skankom skankom = Skankom.getInstance();
 	private ServiceManager() {}
+	private static final Logger LOGGER = Logger.getLogger(ServiceManager.class.getName());
 	
 	public static void manageServices(Announcement announcement) throws IOException {
-		System.out.println("Choose one of the following options:\n1) Add Service \n2) Remove Service \n3) Show Services \n4) Finish");
+		LOGGER.log(Level.INFO,"Choose one of the following options:\n1) Add Service \n2) Remove Service \n3) Show Services \n4) Finish");
 		int choice = SakancomApp.scanInt();
 		switch (choice) {
 		case 1:
-			System.out.println("Enter Servcie:");
+			LOGGER.log(Level.INFO,"Enter Servcie:");
 			String service = SakancomApp.getScanner().nextLine();
 			announcement.addService(service);
-			System.out.println("\"" + service + "\" service is added to " + announcement.toString());
+			LOGGER.log(Level.INFO,"\"" + service + "\" service is added to " + announcement.toString());
 			manageServices(announcement);
 			break;
 		case 2:
 			String serviceInfo = deleteService(announcement);
 			if (serviceInfo != null) {
-				System.out.println("\"" + serviceInfo + "\" service is deleted from " + announcement.toString());
+				LOGGER.log(Level.INFO,"\"" + serviceInfo + "\" service is deleted from " + announcement.toString());
 			}
 			manageServices(announcement);
 			break;
@@ -33,7 +37,7 @@ public class ServiceManager {
 		case 4:
 			break;
 		default:
-			System.out.println("Wrong choice.\n1) Try Again 2) Finish");
+			LOGGER.log(Level.INFO,"Wrong choice.\n1) Try Again 2) Finish");
 			if (SakancomApp.scanInt() == 1) {
 				manageServices(announcement);
 			}
@@ -42,27 +46,27 @@ public class ServiceManager {
 
 	public static void showServices(Announcement announcement) {
 		if (announcement.getServices().isEmpty()) {
-			System.out.println("No srevices added yet to " + announcement.toString());
+			LOGGER.log(Level.INFO,"No srevices added yet to " + announcement.toString());
 			return;
 		}
-		System.out.println("Services in " + announcement.toString() + " are: [");
+		LOGGER.log(Level.INFO,"Services in " + announcement.toString() + " are: [");
 		for (String service: announcement.getServices()) {
-			System.out.println(service);
+			LOGGER.log(Level.INFO,service);
 		}
-		System.out.println("]");
+		LOGGER.log(Level.INFO,"]");
 	}
 	
 	public static String removeService(List<String> list, Announcement announcement) throws IOException {
-		System.out.println("Enter Service Number:");
+		LOGGER.log(Level.INFO,"Enter Service Number:");
 		int serviceNumber = SakancomApp.scanInt();
 		if (serviceNumber > list.size()) {
-			System.out.println("The entered number is not exist.\n1) Try Again \n2) Cancel");
+			LOGGER.log(Level.INFO,"The entered number is not exist.\n1) Try Again \n2) Cancel");
 			if (SakancomApp.scanInt() == 1) {
 				return removeService(list, announcement);
 			}
 			return null;
 		}
-		System.out.println("1) Remove \n2) Cancel");
+		LOGGER.log(Level.INFO,"1) Remove \n2) Cancel");
 		Boolean remove = SakancomApp.scanInt() == 1;
 		if (!remove) { return null; }
 		String service = list.get(serviceNumber - 1);
@@ -73,12 +77,12 @@ public class ServiceManager {
 	
 	public static String deleteService(Announcement announcement) throws IOException {
 		if (announcement.getServices().isEmpty()) {
-			System.out.println("No services added yet to " + announcement.toString());
+			LOGGER.log(Level.INFO,"No services added yet to " + announcement.toString());
 			return null;
 		}
 		int counter = 1;
 		for (String service: announcement.getServices()) {
-			System.out.println(counter + ") " + service);
+			LOGGER.log(Level.INFO,counter + ") " + service);
 			counter++;
 		}
 		return removeService(announcement.getServices(), announcement);
